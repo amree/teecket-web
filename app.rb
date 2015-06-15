618203cac65ac0d44c24e49f4275f5ebb835a6e1
amree <mohd.amree@gmail.com>
2015-06-15 13:12:20 +0800

    Big <censored> commit

--- app.rb
+++ app.rb
@@ -1,5 +1,57 @@
-require 'sinatra'
+require "sinatra/base"
+require "tilt/erubis"
+require "teecket"
 
-get '/' do
-  erb :index
+class App < Sinatra::Base
+  configure do
+    enable :logging
+    file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+    file.sync = true
+    use Rack::CommonLogger, file
+  end
+
+  before do
+    @params = params
+
+    @flights = []
+    @errors = []
+  end
+
+  get '/' do
+    index
+  end
+
+  get '/:from/:to/:date' do
+    index
+  end
+
+  private
+
+  def index
+    validate
+
+    if @errors.empty?
+      search
+    end
+
+    erb :index
+  end
+
+  def validate
+    @params[:from] = @params[:from] || ""
+    @params[:to] = @params[:to] || ""
+    @params[:date] = @params[:date] || ""
+
+    if @params[:submit] && (@params[:from].empty? || @params[:to].empty? || @params[:date].empty?)
+      @errors << "Make sure you've entered all the inputs"
+    end
+  end
+
+  def search
+    unless @params[:submit].nil?
+      @flights = Teecket.search(from: @params[:from], to: @params[:to], date: @params[:date])
+    end
+  end
+
+  run! if app_file == $0
 end
